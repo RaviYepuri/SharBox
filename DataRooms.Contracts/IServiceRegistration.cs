0f62a33d01a1c1bcfe020b186f9c2c63640ce32d
RaviYepuri <128703197+RaviYepuri@users.noreply.github.com>
2026-03-22 22:42:39 +0530

    Add files via upload

---
+++ DataRooms.Contracts/IServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataRooms.Contracts
+{
+    public interface IServiceRegistration
+    {
+        public void RegisterAppServices(IServiceCollection services, IConfiguration config);
+    }
+}
