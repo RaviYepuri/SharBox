47f3659b1f6d4a1a0a377eeb84ed7ea5391f4e28
RaviYepuri <128703197+RaviYepuri@users.noreply.github.com>
2026-03-22 22:59:59 +0530

    Add files via upload

---
+++ LoggerModule/ILoggerManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggerModule
+{
+    public interface ILoggerManager
+    {
+        void LogError(string message, Exception ex);
+        void LogDebug(string message);
+        void LogInfo(string message);
+        void LogWarning(string message);
+    }
+}
---
+++ LoggerModule/LoggerManager.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerModule
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _logger.Warn(message);
+        }
+    }
+}
