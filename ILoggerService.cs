951a9743b57dcb04a2e85a11e9eea1f2438c6ebc
ysankar74@gmail.com <Sankar5591@>
2026-03-22 22:35:12 +0530

    adding code

---
+++ ILoggerService.cs
@@ -0,0 +1,17 @@
+﻿using DataRooms.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataRooms.Contracts
+{
+    public interface ILoggerService
+    {
+        public Task<IEnumerable<ActivityLog>> GetActivityLogs(string searchString);
+        public Task<ActivityLog> GetActivityLog(int id);
+        public Task<int> SaveActivityLog(ActivityLog log);
+        public Task SaveDataLog(DataLog log);
+        public Task<IEnumerable<DataLog>> GetDataLogs(int activityLogIg, string searchString);
+    }
+}
