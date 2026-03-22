211a716fe02f2f2dc428dae24f1a65b68846afff
ysankar74@gmail.com <Sankar5591@>
2026-03-22 23:29:27 +0530

    add code

---
+++ DataRooms.UI/Code/LogWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace DataRooms.UI.Code
+{
+    public class LogWriter
+    {
+        private static string m_exePath = string.Empty;
+        //public LogWriter(string logMessage)
+        //{
+        //    LogWrite(logMessage);
+        //}
+        public static void LogWrite(string logMessage)
+        {
+            m_exePath = System.Web.HttpContext.Current.Server.MapPath("/") + "//Logs";
+            try
+            {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public static void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.WriteLine(logMessage);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
